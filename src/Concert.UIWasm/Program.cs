c87a0e672cf2cbd3c9dfd250c4af76d8944f8715
Alberto <alberto.izquierdomo@gmail.com>
2024-12-06 17:07:04 +0100

    Create SongRequests page and display all in a quickgrid

--- src/Concert.UIWasm/Program.cs
+++ src/Concert.UIWasm/Program.cs
@@ -1,3 +1,4 @@
+using Concert.DataAccess.InMemory.Repositories;
 using Concert.UIWasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddScoped<ISongRequestsRepository, SongRequestsIMRepository>();
 
 await builder.Build().RunAsync();
