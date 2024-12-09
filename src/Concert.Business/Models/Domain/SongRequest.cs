e2b535e424059329d03c5c743fa33f6fc30a73fe
Alberto <alberto.izquierdomo@gmail.com>
2024-12-09 16:23:34 +0100

    Add validation to SongRequest

--- src/Concert.Business/Models/Domain/SongRequest.cs
+++ src/Concert.Business/Models/Domain/SongRequest.cs
@@ -5,9 +5,16 @@ namespace Concert.Business.Models.Domain
     public class SongRequest
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Artist { get; set; }
+        [MaxLength(100)]
         public string Title { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Genre { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Language { get; set; }
     }
 }
