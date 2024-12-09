ef455f7f840c37a56fa52636c4142e0e694e43ff
Alberto <alberto.izquierdomo@gmail.com>
2024-12-09 16:23:22 +0100

    Add number of pages select and edit grid style for SongRequest

---
+++ src/Concert.Business/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concert.Business
+{
+    public static class Constants
+    {
+        public const int SONGREQUEST_ITEMS_PER_PAGE_OPTION_1 = 5;
+        public const int SONGREQUEST_ITEMS_PER_PAGE_OPTION_2 = 10;
+        public const int SONGREQUEST_ITEMS_PER_PAGE_OPTION_3 = 15;
+    }
+}
