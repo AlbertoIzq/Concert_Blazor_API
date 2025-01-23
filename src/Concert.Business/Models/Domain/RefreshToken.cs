985c7b0dbf925d04df1d484074ea6ebc207d43ab
Alberto <alberto.izquierdomo@gmail.com>
2025-01-23 13:21:00 +0100

    Add RefreshToken to AuthDb

---
+++ src/Concert.Business/Models/Domain/RefreshToken.cs
@@ -0,0 +1,10 @@
+﻿namespace Concert.Business.Models.Domain
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Value { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
