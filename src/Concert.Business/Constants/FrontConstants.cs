8907fd9e94f21139c8c391bc9a5ae0f53f226cac
Alberto <alberto.izquierdomo@gmail.com>
2025-01-31 16:14:40 +0100

    Add placeholders to AddEditSongRequest page

--- src/Concert.Business/Constants/FrontConstants.cs
+++ src/Concert.Business/Constants/FrontConstants.cs
@@ -6,5 +6,11 @@
         public const int SONGREQUEST_ITEMS_PER_PAGE_OPTION_2 = 10;
         public const int SONGREQUEST_ITEMS_PER_PAGE_OPTION_3 = 15;
         public const int TOAST_NOTIFICATION_TIMEOUT_SEC = 3;
+
+        public const string SONGREQUEST_EX_ARTIST = "Soundaker";
+        public const string SONGREQUEST_EX_TITLE = "Chased by demons";
+        public const string SONGREQUEST_EX_GENRE = "Melodic death metal";
+        public const string SONGREQUEST_EX_LANGUAGE = "English";
+        public const string PLACEHOLDER_EX_TEXT = "e.g. ";
     }
 }
